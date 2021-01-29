12794f99a891344a130139a9c74bffb2b4f49e79
lingp <carl.lin@huolala.cn>
2021-01-29 23:13:57 +0800

    新增文章

---
+++ src/pages/article/ArticleAdd.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ArticleAdd"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/article/ArticleList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div style="color:black">
+      articlehello
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Article'
+}
+</script>
+
+<style scoped>
+
+</style>
